be248554fcef1e26eee9502d213d8d25a72b56c0
Timothy Cullen <timothycullen@Timothys-MacBook-Pro.local>
2017-10-28 16:06:22 -0700

    still working on the plan

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 'user strict';
 console.log('it lives!');
+
+//begin with funciton to generate random value
+//assign that value to represent imgs
+// build constructor to push into array the results
+// compile and present data of clicks into a readable graph or page?
